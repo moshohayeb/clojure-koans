555f6a91852376e71672cdce4ce89a92388d25bb
Mohammed Alshohayeb <moshohayeb@gmail.com>
2017-09-12 10:27:37 +0300

    solutions

--- src/koans/10_runtime_polymorphism.clj
+++ src/koans/10_runtime_polymorphism.clj
@@ -10,19 +10,19 @@
                    "!")))
 
 (defmulti diet (fn [x] (:eater x)))
-(defmethod diet :herbivore [a] __)
-(defmethod diet :carnivore [a] __)
-(defmethod diet :default [a] __)
+(defmethod diet :herbivore [a] (str (:name a) " eats veggies."))
+(defmethod diet :carnivore [a] (str (:name a) " eats animals."))
+(defmethod diet :default [a] (str "I don't know what " (:name a) " eats."))
 
 (meditations
   "Some functions can be used in different ways - with no arguments"
-  (= __ (hello))
+  (= "Hello World!" (hello))
 
   "With one argument"
-  (= __ (hello "world"))
+  (= "Hello, you silly world." (hello "world"))
 
   "Or with many arguments"
-  (= __
+  (= "Hello to this group: Peter, Paul, Mary!"
      (hello "Peter" "Paul" "Mary"))
 
   "Multimethods allow more complex dispatching"
@@ -40,3 +40,11 @@
   "You may use a default method when no others match"
   (= "I don't know what Rich Hickey eats."
      (diet {:name "Rich Hickey"})))
+
+
+(defmulti z-multi-func (fn [v1 v2] (keyword (str "dispatch-" (rand-int 3)))))
+(defmethod z-multi-func :dispatch-0 [_ _] (println "z-multi-func 0"))
+(defmethod z-multi-func :dispatch-1 [_ _] (println "z-multi-func 1"))
+(defmethod z-multi-func :dispatch-2 [_ _] (println "z-multi-func 2"))
+(defmethod z-multi-func :default [_ _] (println "default z-multi-func"))
+(z-multi-func 3 4)
